f63005bfaab5f93ec739357f30e164feb29db0b9
Dylan Baker <dylanmbaker@yahoo.com>
2022-03-01 20:39:53 -0600

    heroku push

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -2,12 +2,22 @@ exports.up = async (knex) => {
   await knex.schema
     .createTable('users', (users) => {
       users.increments('user_id')
-      users.string('username', 200).notNullable()
+      users.string('username', 200).notNullable().unique()
+      users.string('phoneNumber', 15).notNullable()
       users.string('password', 200).notNullable()
       users.timestamps(false, true)
+    })
+    .createTable('plants', (plant) => {
+      plant.increments('plant_id')
+      plant.string('nickname').notNullable()
+      plant.string('species').notNullable()
+      plant.string('h2oFrequency').notNullable()
+      plant.string('image')
     })
 }
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
+  await knex.schema
+    .dropTableIfExists('plants')
+    .dropTableIfExists('users')
 }
